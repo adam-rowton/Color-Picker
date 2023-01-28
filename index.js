cd97a192f78f3b9c4760c6cdd5bdafa8799c8e45
Adam Rowton <adamlrowton@gmail.com>
2023-01-27 19:56:16 -0800

    MVP

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 const colorBtn = document.getElementById('get-colors-btn')
 
+const colorDisplayGrid = document.getElementById('color-display-grid')
+
+
+colorBtn.addEventListener('click', function () {
+    let selectorInput = document.getElementById('color-selector').value
+    let seedColor = document.getElementById('color-preview-box').value.slice(1)
+    
+    getColors(seedColor, selectorInput)
+})
 
 
 
-
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=monochrome')
+function getColors(seedColor, palette) {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${palette}`)
     .then(res => res.json())
     .then(data => {
-        console.log((data.colors))
-        getColorArray(data.colors)
+        
+        let colorArray = data.colors.map(color => color.hex.value)
+        
+        renderColors(colorArray)
+    })}
 
 
+function renderColors(hexValues) {
+    let colorsHtml = ""
+    hexValues.forEach(hex => {
+        colorsHtml += `
+        <div class="color-container">
+        <div class="color" style="background-color:${hex}">
 
-
+        </div>
+        <div class="hex-value-container">
+            ${hex}
+        </div>
+</div>`
     })
-
-function getColorArray(colors) {
-    let { hex } = colors
-
-    console.log(hex)
+   
+    colorDisplayGrid.innerHTML = colorsHtml
 }
 
-function returnHex() {
-
-}
